a30c5c410628e93b94696a9131ec260c4f9c6477
izyanrosni <62043922+izyanrosni@users.noreply.github.com>
2023-04-05 17:26:18 +0800

    Add files via upload

---
+++ androidTest/java/com/example/i_animalsearcher/ExampleInstrumentedTest.java
@@ -0,0 +1,24 @@
+package com.example.i_animalsearcher;
+
+import android.content.Context;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.example.i_animalsearcher", appContext.getPackageName());
+    }
+
+
+}
